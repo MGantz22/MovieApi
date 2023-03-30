ff48cfdafcc576ee5fe042c2cc59a32ef49aa298
Mitchell Gantz <mgantz22@icloud.com>
2023-03-30 11:39:24 -0700

    test commit
    Co-authored-by: erintimli01 <erintimli01@users.noreply.github.com>
    Co-authored-by: John Lenz <john.lenz27@gmail.com>
    Co-authored-by: Carl Sustrich <CarlSustrich@carl.sustrich@gmail.com

--- MovieApi/Models/Movie.cs
+++ MovieApi/Models/Movie.cs
@@ -5,6 +5,6 @@ namespace MovieApi.Models
     public int MovieId { get; set; }
     public string Name { get; set; }
     public string ReleaseDate { get; set; }
-    public string Genre {get; set;}
+    public string Genre {get; set;} 
   }
 }
