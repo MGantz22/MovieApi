7e5d9d5fb82543907160ebe1b81e91acfc10467f
Mitchell Gantz <mgantz22@icloud.com>
2023-03-30 11:32:07 -0700

    Add Migrations
    Co-authored-by: erintimli01 <erintimli01@users.noreply.github.com>

---
+++ MovieApi/Migrations/20230330170504_init.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieApi.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Movie",
+                columns: table => new
+                {
+                    MovieId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ReleaseDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    Genre = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movie", x => x.MovieId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movie");
+        }
+    }
+}
---
+++ MovieApi/Migrations/20230330173039_Movies.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieApi.Migrations
+{
+    public partial class Movies : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Movie",
+                table: "Movie");
+
+            migrationBuilder.RenameTable(
+                name: "Movie",
+                newName: "Movies");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ReleaseDate",
+                table: "Movies",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Movies",
+                table: "Movies",
+                column: "MovieId");
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "MovieId", "Genre", "Name", "ReleaseDate" },
+                values: new object[,]
+                {
+                    { 1, "SciFi", "Ready Player One", "2011" },
+                    { 2, "Family", "Land Before Time", "1988" },
+                    { 3, "Fantasy", "Matilda", "1996" },
+                    { 4, "Sci-fi", "Everything Everywhere All at Once", "2022" },
+                    { 5, "Suspense", "I See You", "2019" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Movies",
+                table: "Movies");
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 5);
+
+            migrationBuilder.RenameTable(
+                name: "Movies",
+                newName: "Movie");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ReleaseDate",
+                table: "Movie",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Movie",
+                table: "Movie",
+                column: "MovieId");
+        }
+    }
+}
