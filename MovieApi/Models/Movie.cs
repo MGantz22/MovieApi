c60996fd1b94018d76120c2bc0810fe1a01d8b57
Mitchell Gantz <mgantz22@icloud.com>
2023-03-30 11:32:50 -0700

    Create Movie.cs
    Co-authored-by: erintimli01 <erintimli01@users.noreply.github.com>

---
+++ MovieApi/Models/Movie.cs
@@ -0,0 +1,10 @@
+namespace MovieApi.Models
+{
+  public class Movie
+  {
+    public int MovieId { get; set; }
+    public string Name { get; set; }
+    public string ReleaseDate { get; set; }
+    public string Genre {get; set;}
+  }
+}
